21e0ed6a494e30745f15c84bc7a5089415b0c8e7
FlaviaTG <57419069+FlaviaTG@users.noreply.github.com>
2021-01-06 14:40:42 -0500

    Update beagle-Formating.sh

--- beagle-Formating.sh
+++ beagle-Formating.sh
@@ -16,13 +16,11 @@ module load jdk/1.8.0_172-fasrc01
 module load GCC/7.3.0-2.30 OpenMPI/3.1.1 BCFtools/1.9
 module load jdk/1.8.0_172-fasrc01
 module load vcftools/0.1.14-fasrc01
-#module load GCC/8.2.0-2.31.1 SAMtools/1.9
 module load tabix/0.2.6-fasrc01
-#MB=/n/holylfs/LABS/edwards_lab/ftermignoni/Jclark-reseq/Catharus_genomes/Catmin_final.assembly.fasta.fai
+
 gtool=/n/holyscratch01/edwards_lab/ftermignoni/Jclark-reseq/VCF-genotype/minimus/glactools/glactools
-BPATH=/n/holylfs/LABS/edwards_lab/ftermignoni/programs/beagle.18May20.d20.jar
 REFPATH=/n/holyscratch01/edwards_lab/ftermignoni/Jclark-reseq/BWA-chr/pilonRESULTS/pilon.fasta.fai
 
-#$gtool beagle2glf --fai $REFPATH genolike_34_minimus-AUTOSOME > genolike_34_minimus-AUTOSOME.glf.gz
-#$gtool glf2acf genolike_34_minimus-AUTOSOME.glf.gz > genolike_34_minimus-AUTOSOME.acf.gz
+$gtool beagle2glf --fai $REFPATH genolike_34_minimus-AUTOSOME > genolike_34_minimus-AUTOSOME.glf.gz \
+$gtool glf2acf genolike_34_minimus-AUTOSOME.glf.gz > genolike_34_minimus-AUTOSOME.acf.gz \
 $gtool glac2vcf genolike_34_minimus-AUTOSOME.acf.gz > genolike_34_minimus-AUTOSOME.vcf
